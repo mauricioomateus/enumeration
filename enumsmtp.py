f852b492a6539c3225c450a5eed871cb3db1d833
Mauricio Mateus <mauriciomateus_13@hotmail.com>
2022-01-06 20:57:23 -0300

    Update enumsmtp.py

--- enumsmtp.py
+++ enumsmtp.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python
 
-import socket,sys
+import socket,sys,re
 
-if len(sys.argv) != 3:
-        print "Modo de uso: python3 enumsmtp.py IP user"
-        sys.exit(0)
+if len(sys.argv) != 2:
+        print "Modo de uso: python3 enumsmtp.py IP"
+        sys.exit(0) 
 
-tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp.connect((sys.argv[1],25))
-
-banner = tcp.recv(1024)
-print (banner)
-
-tcp.sendall(b'VRFY '+sys.argv[2]+'\r\n')
-usr = tcp.recv(1024)
-print (user)
+#abrir um aquivo com uma lista de usuarios
+file = open("lista.txt") 
+for linha in file:  
+        tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        tcp.connect((sys.argv[1],25))
+        banner = tcp.recv(1024)
+        #print (banner)
+        
+        tcp.sendall(b'VRFY '+linha)
+        usr = tcp.recv(1024)
+                #buscando por '252'
+                if re.search("252",user):
+                        print ("Usaurio encontrado: "+user).strip("252 2.0.0")
